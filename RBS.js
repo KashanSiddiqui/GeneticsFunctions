dbabc76e0e6dafda0b84a12e4d0b07a9ff9f83b7
maaz28 <maazjawaid28@gmail.com>
2019-03-16 18:42:10 +0500

    initial

--- RBS.js
+++ RBS.js
@@ -17,6 +17,11 @@ function random_number_for_z() {
     return random;
 }
 
+function random_number_between_zero_and_one() {
+    var random = (Math.random() * 1);
+    return random;
+}
+
 const population = (initial_population_size, gene_count) => { // calculate initial population
     let array = [];
     for (let i = 0; i < initial_population_size; i++) {
@@ -45,7 +50,7 @@ const total_rank_calculator = (array) =>  {
     for (let i = 0; i < array.length; i++) {
         sum += (i+1);
     }
-    console.log(sum, "sum");
+    // console.log(sum, "sum");
     return sum;
 } 
 
@@ -53,16 +58,56 @@ const RBS = (array) => {
     let relative_boundries_array = [];
 let total_rank_value = total_rank_calculator(array);
 relative_boundries_array.push( 1 / total_rank_value );
-console.log(relative_boundries_array); // special case for 1st array
+// console.log(relative_boundries_array); // special case for 1st array
 for(var i=1; i<array.length; i++){
     let value = (i+1) / total_rank_value;
-    console.log(value, "******** value ************");
+    // console.log(value, "******** value ************");
      relative_boundries_array[i] = relative_boundries_array[i-1] + value;
 }
 return relative_boundries_array;
 }
 
-let initial_population_arr = population(3, 3);
+const parent_index = (RBS_array, parent) => {
+    for (let j = 0; j < RBS_array.length; j++) {
+        if(! (parent > RBS_array[j])){
+            return j;
+        }                
+    }
+}
+
+const crossover = (array, parent1, parent2) => {
+    let children_array = [];
+    children_array[0] = array[parent1];
+    children_array[1] = array[parent2];
+    children_array[0][0] = children_array[0][0] + children_array[1][0];
+    children_array[1][0] = children_array[0][0] - children_array[1][0];
+    children_array[0][0] = children_array[0][0] - children_array[1][0];
+    return (children_array)
+}
+
+const children_production_function = (initial_array, RBS_array) => {
+    let children_array = [];
+    for (let i = 0; i < initial_array.length / 2; i++) {        
+        let parent1 = random_number_between_zero_and_one();
+        let parent2 = random_number_between_zero_and_one();
+        if(parent1 === parent2){
+            i--;
+        }else{
+            parent1 = parent_index(RBS_array, parent1)
+            parent2 = parent_index(RBS_array, parent2)
+            console.log(parent1, parent2)
+        children_array = children_array.concat(crossover(initial_array, parent1, parent2));
+    }
+}
+console.log(children_array.length);
+    }
+    
+
+
+
+let initial_population_arr = population(20, 3); // 1st param size, 2nd pram gene size
 // let total_fitness_value = total_fitness_calculator(initial_population_arr);
 let RBS_array = RBS( initial_population_arr);
-console.log(RBS_array);
+let children_array = children_production_function(initial_population_arr, RBS_array) 
+
+// console.log(RBS_array);
